6ac56b1a71b70a354a6edd77df3448d9b0e4b631
mikasaukyou <43193913+mikasaukyou@users.noreply.github.com>
2024-07-14 20:38:46 +0900

    Update Lyra2REVc0ban.c

--- src/Lyra2REVc0ban.c
+++ src/Lyra2REVc0ban.c
@@ -1,6 +1,18 @@
 #include "Lyra2RE.h"
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+#include "Lyra2.h"
 
 void lyra2revc0ban_hash(const char* input, char* output)
 {
-  lyra2rec0ban_hash(input, output);
+
+    uint32_t hashB[8];
+
+
+	LYRA2((void*)hashB, 32, (const void*)input, 80, (const void*)input, 80, 2, 330, 256);
+
+
+	memcpy(output, hashB, 32);
 }
